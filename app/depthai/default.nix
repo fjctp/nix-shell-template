ef1b0f3f9bc8ff666ca06de7141c00849002ec20
fjctp <snchan20@yahoo.com>
2025-03-23 16:01:36 -0700

    feat: app/depthai

---
+++ app/depthai/default.nix
@@ -0,0 +1,36 @@
+# Python dev FHS environment, customized for [DepthAI](https://github.com/luxonis/depthai)
+#
+# See also
+# - https://github.com/gfx-rs/wgpu/issues/3033#issuecomment-1741255540
+# - https://docs-old.luxonis.com/en/latest/pages/troubleshooting/#udev-rules-on-linux
+# - https://docs-old.luxonis.com/en/latest/pages/troubleshooting/
+# - https://github.com/luxonis/depthai/issues/518
+
+{ pkgs ? import <nixpkgs> {} }: 
+(pkgs.buildFHSUserEnv {
+  name = "depthAI";
+  targetPkgs = pkgs: (with pkgs; [
+    # Python
+    python312
+    python312Packages.pip
+    # Numpy
+    gcc
+    glibc
+    zlib
+    # Display
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    wayland
+    libxkbcommon
+    # Drivers (WGPU Error)
+    libGL
+    vulkan-loader
+    # libgthread
+    glib
+    # lsusb
+    usbutils
+  ]);
+  runScript = "bash";
+}).env
